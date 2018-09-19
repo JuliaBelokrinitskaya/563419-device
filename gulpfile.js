1530b0fe89b346a1a2aab04d8735deeaab4e8947
Julia Belokrinitskaya <bel_julia@mail.ru>
2018-09-19 21:19:49 +0300

    Add build and serve tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@
 
 var
   gulp = require('gulp'),
-  del = require('del')
+  del = require('del'),
+
+  run = require('run-sequence'),
+  server = require('browser-sync').create()
 ;
 
 gulp.task('clean', function() {
@@ -13,4 +16,21 @@ gulp.task('clean', function() {
     'js',
     '*.html'
   ]);
+});
+
+gulp.task('build', function (done) {
+  return run(
+    'clean',
+    done
+  );
+});
+
+gulp.task('serve', function () {
+  server.init({
+    server: '.',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
 });
