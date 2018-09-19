e9b765ab230809560064130d9f4ce9d625c9f3bd
Julia Belokrinitskaya <bel_julia@mail.ru>
2018-09-20 00:02:11 +0300

    Add html task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@
 var
   gulp = require('gulp'),
   del = require('del'),
+  plumber = require('gulp-plumber'),
+
+  pug = require('gulp-pug'),
+  htmlmin = require('gulp-htmlmin'),
 
   run = require('run-sequence'),
   server = require('browser-sync').create()
@@ -18,9 +22,24 @@ gulp.task('clean', function() {
   ]);
 });
 
+gulp.task('html', function () {
+  return gulp.src('src/*.pug')
+    .pipe(plumber())
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('src'))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('.'))
+    .pipe(server.stream());
+});
+
 gulp.task('build', function (done) {
   return run(
     'clean',
+    'html',
     done
   );
 });
@@ -33,4 +52,6 @@ gulp.task('serve', function () {
     cors: true,
     ui: false
   });
+
+  gulp.watch('src/**/*.pug', ['html']);
 });
